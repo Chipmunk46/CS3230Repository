a026df52a75ca0800dbc04e4b338fe56a70410e0
BuildTools <unconfigured@null.spigotmc.org>
2021-05-18 20:21:50 -0600

    did thing

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    private String getHello() {
+        return "Hello World";
+    }
+    public static void main(String[] args){
+        String string = getHello();
+    }
+}
